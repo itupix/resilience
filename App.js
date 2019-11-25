1f02e67134a9812f0eabf43aa55850958c92623d
itupix <51956779+itupix@users.noreply.github.com>
2019-11-25 16:10:52 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { OK, LOADING, ERROR, EMPTY, DENIED } from './constants';
 
-export const MyComponent = ({ status }) => (
+export const MyComponent = ({ status, onRetryClick }) => (
   <section>
     <h1>My component title</h1>
     {{
-      [resilienceStatus.OK]: <div>My content is ready to be displayed.</div>,
-      [resilienceStatus.LOADING]: <div>My component is fetching data.</div>
-      [resilienceStatus.ERROR]: <div>Seems like we have a problem.</div>
-      [resilienceStatus.EMPTY]: <div>There is nothing here.</div>,
-      [resilienceStatus.DENIED]: <div>You are not allowed to see this content.</div>
+      [LOADING]: <div>Chargement...</div>,
+      [ERROR]: <div>Une erreur serveur est survenue. <button onClick={onRetryClick}>Réessayer</button></div>,
+      [EMPTY]: <div>Aucun élément à afficher.</div>,
+      [DENIED]: <div>Vous n'êtes pas autorisé.e à voir ce contenu</div>,
+      [OK]: <div>Contenu prêt à être affiché.</div>
     }[status]}
   </section>
 );
